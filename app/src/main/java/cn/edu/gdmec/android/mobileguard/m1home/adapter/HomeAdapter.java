3f59d61c1e6fddc8435b3772ddca15ee88c69eab
ZFJaLLE <h1021858946@126.com>
2017-09-24 23:22:12 +0800

    敲了好多代码，心酸

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/adapter/HomeAdapter.java
@@ -0,0 +1,8 @@
+package cn.edu.gdmec.android.mobileguard.m1home.adapter;
+
+/**
+ * Created by Personal on 2017/9/24.
+ */
+
+public class HomeAdapter {
+}
---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/entity/VersionEntity.java
@@ -0,0 +1,11 @@
+package cn.edu.gdmec.android.mobileguard.m1home.entity;
+
+/**
+ * Created by Personal on 2017/9/24.
+ */
+
+public class VersionEntity {
+    public  String versionCode;
+    public  String description;
+    public  String apkurl;
+}
