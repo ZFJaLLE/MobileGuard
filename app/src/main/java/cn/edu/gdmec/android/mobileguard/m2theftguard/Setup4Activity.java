8ff536a62f6593cb7ce5f8448f74a6ed2a20a422
ZFJaLLE <h1021858946@126.com>
2017-10-13 13:58:49 +0800

    第二周自动化完成

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/Setup4Activity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/Setup4Activity.java
@@ -2,14 +2,28 @@ package cn.edu.gdmec.android.mobileguard.m2theftguard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 import cn.edu.gdmec.android.mobileguard.R;
 
-public class Setup4Activity extends AppCompatActivity {
+public class Setup4Activity extends BasicSetUpActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setup4);
+        setContentView(R.layout.activity_setup_4);
+
+        ((RadioButton) findViewById(R.id.rb_four)).setChecked(true);
+    }
+
+    @Override
+    public void showNext(){
+        Toast.makeText(this, "当前页面已经是最后一页", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void showPre(){
+        startActivityAndFinishSelf(Setup3Activity.class);
     }
 }
