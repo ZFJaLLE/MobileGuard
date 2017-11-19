2d1ef91ee1f84e2181974a56bed8ddff1cd627e0
guoqinfeng <172294903@qq.com>
2017-11-19 22:23:09 +0800

    郭钦锋

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/utils/MyUtils.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/utils/MyUtils.java
@@ -1,9 +1,15 @@
 package cn.edu.gdmec.android.mobileguard.m1home.utils;
 
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Environment;
+
+import java.io.File;
 
 /**
  * Created by Personal on 2017/9/24.
@@ -20,4 +26,17 @@ public class MyUtils {
             return "";
         }
     }
+    /**
+     * 安装新版本
+     * @param activity
+     */
+    public static void installApk(Activity activity, String apkFile){
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_DEFAULT);
+        intent.setDataAndType(Uri.fromFile(
+                new File(Environment.getExternalStoragePublicDirectory("/download/").getPath()+"/"+apkFile)),"application/vnd.android.package-archive");
+        activity.startActivityForResult(intent,0);
+    }
+
+
 }
--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m5virusscan/dao/AntiVirusDao.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m5virusscan/dao/AntiVirusDao.java
@@ -17,26 +17,35 @@ public class AntiVirusDao {
      */
     private static Context context;
     private static String dbname;
-
-    public AntiVirusDao(Context context) {
-        this.context = context;
-        dbname = "data/data/" + context.getPackageName() + "/files/antivirus.db";
+    public AntiVirusDao(Context context){
+        this.context =context;
+        dbname ="/data/data/"+context.getPackageName()+"/files/antivirus.db";
     }
-
-    //使用apk文件的md5值匹配病毒数据库
     public String checkVirus(String md5) {
         String desc = null;
-        //打开数据库
         SQLiteDatabase db = SQLiteDatabase.openDatabase(
-                dbname, null, SQLiteDatabase.OPEN_READONLY);
-        Cursor cursor = db.rawQuery("select desc form datable where msf=?", new
-                String[]{md5});
+                dbname, null,
+                SQLiteDatabase.OPEN_READONLY);
+        Cursor cursor = db.rawQuery("select desc from datable where md5=?",
+                new String[] { md5 });
         if (cursor.moveToNext()) {
             desc = cursor.getString(0);
         }
-
         cursor.close();
         db.close();
         return desc;
-}
+    }
+    public String getVirusDbVersion(){
+        String dbVersion = null;
+        SQLiteDatabase db = SQLiteDatabase.openDatabase(
+                dbname, null,
+                SQLiteDatabase.OPEN_READONLY);
+        Cursor cursor = db.rawQuery("select major||'.'||minor||'.'||build from version",null);
+        if (cursor.moveToNext()) {
+            dbVersion = cursor.getString(0);
+        }
+        cursor.close();
+        db.close();
+        return dbVersion;
+    }
 }
